14158a653f0cae90f1ad859f88fa316ffc23ff6c
GustavoRibeiro01 <36051926+GustavoRibeiro01@users.noreply.github.com>
2019-10-07 10:31:45 -0300

    Gravar Empresa

---
+++ API/Data Transfer Object/Empresa/DtoEmpresa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Data_Transfer_Object.Empresa
+{
+    public class DtoEmpresa
+    {
+        public int id { get; set; }
+        public string nome_usuario { get; set; }
+        public string senha { get; set; }
+        public string email { get; set; }
+        public int id_grupo { get; set; }
+        public int id_usuario { get; set; }
+        public string nome { get; set; }
+        public string razao { get; set; }
+        public string cidade { get; set; }
+        public string ramo { get; set; }
+
+    }
+}
--- API/Repositorio/Empresa/EmpresaRep.cs
+++ API/Repositorio/Empresa/EmpresaRep.cs
@@ -1,14 +1,57 @@
-﻿using Dapper;
+﻿using API.Data_Transfer_Object.Empresa;
+using Dapper;
+using API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using API.Repositorio.Usuario;
+using System.Transactions;
 
 namespace API.Repositorio.Empresa
 {
     public class EmpresaRep : RepositorioBase<API.Models.Empresa>
     {
         private string query = "";
+
+        public void GravarEmpresa(DtoEmpresa dtoempresa)
+        {
+            try
+            {
+                using (TransactionScope tscope = new TransactionScope())
+                {
+
+                   var usuario = new Models.Usuario();
+                    var empresa = new Models.Empresa();
+
+                    usuario.nome = dtoempresa.nome;
+                    usuario.email = dtoempresa.email;
+                    usuario.senha = dtoempresa.senha;
+                    usuario.id_grupo = dtoempresa.id_grupo;
+
+                    using (var db = new UsuarioRep())
+                    {
+                        empresa.id_usuario = (int) db.Insert(usuario);
+                    }
+
+                    empresa.nome = dtoempresa.nome;
+                    empresa.ramo = dtoempresa.ramo;
+                    empresa.razao = dtoempresa.razao;
+                    empresa.cidade = dtoempresa.cidade;
+
+                    using (var db = new EmpresaRep())
+                    {
+                        db.Insert(empresa);
+                    }
+
+                    tscope.Complete();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
 
         public void Delete(int id)
         {
